47e807242c510b17df3a69d8a1179fb5dfae3de2
Vinay Yadav <vinay.manoj.yadav491@gmail.com>
2025-03-16 09:24:15 +0530

    add

--- router/userRouter.js
+++ router/userRouter.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const passport = require("passport");
 const UserController = require("../controller/user.js");
 
-router.route("/signup")
-  .get((req, res) => {
-    res.render("../views/pages/signup.ejs");
-  })
-  .post((req, res) => {
-    UserController.signUp(req, res); // Handle user signup
-  });
+// router.route("/signup")
+//   .get((req, res) => {
+//     res.render("../views/pages/signup.ejs");
+//   })
+  // .post((req, res) => {
+  //   UserController.signUp(req, res); // Handle user signup
+  // });
 
 router.route("/login")
   .get((req, res) => {
-    res.render("../views/pages/login.ejs", { cssFile: "/css/style.css" });
+    res.render("../views/pages/login.ejs");
   })
   .post(
     passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }),
@@ -26,28 +26,41 @@ router.route("/home")
   .get((req, res) => {
     res.render("../views/pages/home.ejs");
   });
+  router.route("/insurance")
+  .get((req, res) => {
+    res.render("../views/pages/insurance.ejs");
+  });
+  // router.route("/home")
+  // .get((req, res) => {
+  //   res.render("../views/pages/home.ejs");
+  // });
 
 router.route("/stock")
   .get((req, res) => {
     res.render("../views/pages/stock.ejs");
   });
 
-// Profile route to view the user profile
-router.route("/profile/:id")
-  .get(async (req, res) => {
-    try {
-      const { id } = req.params; // Use req.params.id to get the user ID
-      const user = await UserController.getUserById(id); // Get user data from the controller
-
-      if (!user) {
-        return res.status(404).send("User not found"); // Handle user not found
-      }
-
-      res.render("../views/pages/profile.ejs", { user }); // Pass the user object to the view
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-    }
+  router.route("/profile")
+  .get((req, res) => {
+    res.render("../views/pages/profile.ejs");
   });
+
+// Profile route to view the user profile
+// router.route("/profile/:id")
+//   .get(async (req, res) => {
+//     try {
+//       const { id } = req.params; // Use req.params.id to get the user ID
+//       const user = await UserController.getUserById(id); // Get user data from the controller
+
+//       if (!user) {
+//         return res.status(404).send("User not found"); // Handle user not found
+//       }
+
+//       res.render("../views/pages/profile.ejs", { user }); // Pass the user object to the view
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send("Internal Server Error");
+//     }
+//   });
 
 module.exports = router;
